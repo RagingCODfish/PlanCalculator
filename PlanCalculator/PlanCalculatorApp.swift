d5d7bbcea7695e03b0ca824a3f384f88b7bd6ebb
RagingCODfish <zachary.uptin@me.com>
2022-10-08 18:01:46 +1000

    Initial Commit

---
+++ PlanCalculator/PlanCalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  PlanCalculatorApp.swift
+//  PlanCalculator
+//
+//  Created by Zach Uptin on 8/10/2022.
+//
+
+import SwiftUI
+
+@main
+struct PlanCalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
